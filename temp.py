5f25f7591a0c0707f65e5f817b8836ac3c818b4d
YvonneXia <33067909+YvonneXia@users.noreply.github.com>
2018-02-12 09:14:18 -0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,11 +4,13 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+import csv
 import pandas as pd
-import math
+import numpy as np
 
-df = pd.read_csv('/Users/yuanxia/Desktop/angles_UCI_CS.csv', sep=',')
-df.loc[:,' angle_cosine'] = df.loc[:,' angle_degrees'].apply(lambda x: math.cos(x*math.pi/180))
-df.to_csv('/Users/yuanxia/Desktop/out.csv', sep = ',',index=False)
+df = pd.read_csv('angles_UCI_CS.csv')
+df[' angle_cosine']=np.cos(df[' angle_degrees'])
+#this is another method to apply an extra column in output csv
+#df.loc[:,' angle_cosine'] = df.loc[:,' angle_degrees'].apply(lambda x: math.cos(x*math.pi/180))
+df.to_csv('out.csv',index=False)
 
