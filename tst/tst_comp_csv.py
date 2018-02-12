f21cbba3262858d3307ba6d8ae4c921bf4f09e9c
YvonneXia <33067909+YvonneXia@users.noreply.github.com>
2018-02-12 09:25:40 -0800

    Update tst_comp_csv.py

--- tst/tst_comp_csv.py
+++ tst/tst_comp_csv.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+def compare(f1,f2):
+  df1 = pd.read_csv(f1)
+  df2 = pd.read_csv(f2)
+  
+  if df1.equals(df2):
+    return 0
+  else:
+    return 1
+  
+  
+ 
+f1= 'angles_UCI_CS.csv'
+f2 = 'angles_UCI_CS_out.csv'
+
+print(compare(f1,f2))
